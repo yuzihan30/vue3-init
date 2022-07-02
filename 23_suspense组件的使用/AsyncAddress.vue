3ca66f791d49c991bef1ab83ce7bf80318af3338
yuzihan <yuzihanyuzihan@163.com>
2022-07-02 21:19:38 +0800

    feat: vue3中异步组件

---
+++ 23_suspense组件的使用/AsyncAddress.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>AsyncAddress组件</h2>
+  <h3>{{ data }}</h3>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "axios";
+export default defineComponent({
+  name: "AsyncAddress",
+  //   setup() {
+  //     // 可以研究下为何这样写路径为何可以直接找到
+  //     return axios.get("/data/address.json").then((response) => {
+  //       return {
+  //         data: response.data,
+  //       };
+  //     });
+  //   },
+  async setup() {
+    const result = await axios.get("/data/address.json");
+    return {
+      data: result.data,
+    };
+  },
+});
+</script>
---
+++ 23_suspense组件的使用/AsyncComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>AsyncComponent子级组件</h2>
+  <h3>{{ msg }}</h3>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "AsyncComponent",
+  setup() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ msg: "拥抱星星的月亮" });
+      }, 3000);
+    });
+    // return {};
+  },
+});
+</script>
