f20ed04adb072e041f2ab7c4f6d60d7cc1573c50
yuzihan <yuzihanyuzihan@163.com>
2022-07-02 18:47:45 +0800

    feat: Teleport

---
+++ 21_Fragment组件的介绍/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <div>
+    <h2>测试1</h2>
+    <h2>测试2</h2>
+  </div> -->
+  <!-- 在 Vue3 中: 组件可以没有根标签, 内部会将多个标签包含在一个 Fragment 虚拟元素中
+好处: 减少标签层级, 减小内存占用 -->
+  <h2>测试1</h2>
+  <h2>测试2</h2>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+});
+</script>
---
+++ src/ModalButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <button @click="modalOpen = true">打开一个对话框</button>
+  <Teleport to="body">
+    <div v-if="modalOpen" class="modal">
+      <div>
+        这是对话框
+
+        <button @click="modalOpen = false">关闭对话框</button>
+      </div>
+    </div>
+  </Teleport>
+  <!-- 对话框代码 -->
+  <!-- <div v-if="modalOpen">
+    <div>这是对话框</div>
+    <button @click="modalOpen = false">关闭对话框</button>
+  </div> -->
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "ModalButton",
+  setup() {
+    const modalOpen = ref(false);
+    return {
+      modalOpen,
+    };
+  },
+});
+</script>
+<style scoped>
+.modal {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  width: 300px;
+  height: 300px;
+  padding: 5px;
+}
+</style>
